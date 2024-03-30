21aeac53b1a81d703a54fb1623e206d824ce49a6
Luka Mijalkovic <luka.mijalkovic3@gmail.com>
2024-03-29 23:17:40 -0400

    Ported login page to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,136 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled, { css, createGlobalStyle } from 'styled-components'
+import Helonik from './assets/helonik.otf'
+
+const GlobalStyle = createGlobalStyle`
+    @font-face {
+      font-family: helonik;
+      src: url(${Helonik}) format('opentype');
+    }
+    
+    html * {
+        font-family: helonik;
+    }
+    
+    body {
+        background-color: #000000;
+        opacity: 1;
+        background-image: radial-gradient(#222222 2px, #000000 2px);
+        background-size: 40px 40px;
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+    }
+
+    input, button, textarea {
+        border-color: transparent;
+        border-radius: 6px;
+        font-size: 20px;
+        color: black;
+        background: grey;
+    }
+    
+    textarea {
+        resize: vertical;
+    }
+    
+    label {
+        padding-bottom: 2px;
+    }
+`;
+
+const BlurBox = styled.div`
+    background-color: rgba(255, 255, 255, 0.01);
+    -webkit-backdrop-filter: blur(2px);
+    backdrop-filter: blur(2px);
+    border-radius: 20px;
+    border-style: solid;
+    border-color: grey;
+    border-width: thin;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: space-between;
+`;
+
+const InputField = styled.div`
+    width: 400px;
+    font-size: 30px;
+    padding: 10px;
+    font-family: helonik;
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+    color: grey;
+`;
+
+const RememberMe = styled.div`
+    font-size: 30px;
+    font-family: helonik;
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+    color: grey;
+`;
+
+const Or = styled.div`
+    width: 400px;
+    font-size: 20px;
+    padding: 10px;
+    font-family: helonik;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: grey;
+    gap: 10px;
+`;
+
+const Rectangle = styled.div`
+    height: 2px;
+    width: 100%;
+    background: grey;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <GlobalStyle />
+        <BlurBox>
+            <InputField>
+                todo
+            </InputField>
+            <InputField>
+                <label for="uname">username</label>
+                <input type="text" placeholder="" name="uname" required />
+            </InputField>
+            <InputField>
+                <label for="psw">password</label>
+                <input type="password" placeholder="" name="psw" required />
+            </InputField>
+            <InputField>
+                <RememberMe>
+                    <label>
+                      <input type="checkbox" checked="checked" name="remember" /> remember me
+                    </label>
+                </RememberMe>
+            </InputField>
+            <InputField>
+                <button type="submit">login</button>
+            </InputField>
+            <Or>
+                <Rectangle />
+                <div>or</div>
+                <Rectangle />
+            </Or>
+            <InputField>
+                    <button type="submit" onclick="location.href='register.html'">register</button>
+            </InputField>
+        </BlurBox>
     </>
   )
 }
 
-export default App
+export default App;
