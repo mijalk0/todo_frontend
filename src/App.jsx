292a0940967534ff6700a22a8353d7e69c366967
Luka Mijalkovic <luka.mijalkovic3@gmail.com>
2024-04-04 02:57:08 -0400

    Remove dead code in App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import React, { useState, useEffect, useContext } from 'react'
-import AnnotatedCheckbox from './components/AnnotatedCheckbox'
-import OrDivider from './components/OrDivider'
-import { Box, BlurBox, Container as BoxContainer, Item as BoxItem } from './components/Box'
-import Button from './components/Button';
-import { TextArea, SmallInput as SmallTextInput, LargeInput as LargeTextInput } from './components/Text';
-import Task, { TaskHeader } from './components/Task';
-import { RequiredLabel, RequiredText } from './components/Required';
-import Modal, { ModalContext, ModalProvider } from './components/Modal';
+import React from "react";
+import { ModalProvider } from "./components/Modal";
 import LoginPage from "./pages/Login";
 import RegisterPage from "./pages/Register";
 import TasksPage from "./pages/Tasks";
-import { useForm } from 'react-hook-form';
-import styled, { createGlobalStyle } from 'styled-components'
-import Helonik from './assets/helonik.otf'
-import checked from './assets/checkbox-checked-svgrepo-com.svg'
-import unchecked from './assets/checkbox-unchecked-svgrepo-com.svg'
-import { BrowserRouter as Router, Routes, Route, useNavigate, Navigate } from 'react-router-dom';
+import Helonik  from "./assets/helonik.otf";
+import { createGlobalStyle } from "styled-components";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 export { BACKEND_URL };
 
 const GlobalStyle = createGlobalStyle`
@@ -42,37 +32,25 @@ const GlobalStyle = createGlobalStyle`
     label {
         padding-bottom: 2px;
     }
-
-    input[type=checkbox]::before {
-        content: " ";
-        display:inline-block;
-        width:16px;
-        height:16px;
-        background: url(${unchecked});
-    }
-
-    input[type=checkbox]:checked::before {
-        background: url(${checked});
-    }
 `;
 
 function App() {
-  return (
-    <>
-        <GlobalStyle />
-        <Router>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/tasks" element={<TasksPage />}/>
-          </Routes>
-        </Router>
-    </>
-  )
+    return (
+        <>
+            <GlobalStyle />
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/tasks" element={<TasksPage />} />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default () => (
-  <ModalProvider>
-    <App />
-  </ModalProvider>
+    <ModalProvider>
+        <App />
+    </ModalProvider>
 );
