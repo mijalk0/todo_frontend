e0b85d822e5b983f8d612427bc1c9668250c2a67
Luka Mijalkovic <luka.mijalkovic3@gmail.com>
2024-04-04 02:15:27 -0400

    Moved task components into new file

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import Button from './components/Button';
 import TextArea from './components/TextArea';
 import SmallTextInput from './components/SmallTextInput';
 import LargeTextInput from './components/LargeTextInput';
+import Task, { TaskHeader } from './components/Task';
 import { useForm } from 'react-hook-form';
-import debounce from 'debounce';
 import styled, { createGlobalStyle } from 'styled-components'
 import Helonik from './assets/helonik.otf'
 import checked from './assets/checkbox-checked-svgrepo-com.svg'
@@ -66,24 +66,6 @@ const RequiredText = styled.div`
     font-size: 20px;
 `;
 
-const TaskHeader = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    gap: 20px;
-    height: 50px;
-`;
-
-const TaskFooter = styled(TaskHeader)`
-    font-size: 15px;
-`;
-
-const TaskDates = styled.div`
-    display: flex;
-    flex-direction: column;
-`;
-
-
 const ModalContext = React.createContext();
 
 function ModalProvider({ children }) {
@@ -127,86 +109,6 @@ function Modal(props) {
     </ModalBackground>
   );
 };
-
-const updateDescription = debounce((description, setUpdatedAt, id) => {
-        fetch(`${BACKEND_URL}/tasks/${id}`, {
-          method: "PATCH",
-          credentials: 'include',
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-              description: description,
-          })
-        })
-            .then((response) => response.json())
-            .then((data) => setUpdatedAt(data.updatedAt));
-}, 500);
-
-function Task(props) {
-    const [description, setDescription] = useState(props.description);
-    const [completed, setCompleted] = useState(props.completed);
-    const [updatedAt, setUpdatedAt] = useState(props.updatedAt);
-
-    async function handleDescriptionChange(event) {
-        const newDescription = event.target.value;
-        setDescription(newDescription);
-        updateDescription(newDescription, setUpdatedAt, props.id);
-    }
-
-    function handleCompletedChange(event) {
-        const newCompleted = !(event.target.value == 'true');
-        setCompleted(newCompleted);
-        fetch(`${BACKEND_URL}/tasks/${props.id}`, {
-          method: "PATCH",
-          credentials: 'include',
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-              completed: newCompleted,
-          })
-        })
-            .then((response) => response.json())
-            .then((data) => setUpdatedAt(data.updatedAt));
-    }
-
-    useEffect(() => {
-    }, [description]);
-
-    useEffect(() => {
-    }, [completed]);
-
-    return (
-        <BlurBox>
-            <BoxItem>
-                <TaskHeader>
-                    <div>{props.title}</div>
-                        <Button type="submit" onClick={async () => {
-                            await fetch(`${BACKEND_URL}/tasks/${props.id}`, {
-                                method: "DELETE",
-                                credentials: 'include',
-                            });
-                            props.deleteTask(props.id);
-                        }
-                    }>delete</Button>
-                </TaskHeader>
-            </BoxItem>
-            <BoxItem>
-                <TextArea value={description} placeholder="description" onChange={handleDescriptionChange}></TextArea>
-            </BoxItem>
-            <BoxItem>
-                <TaskFooter>
-                    <TaskDates>
-                        <div>created at: {new Date(props.createdAt).toLocaleString('en-US')}</div>
-                        <div>updated at: {new Date(updatedAt).toLocaleString('en-US')}</div>
-                    </TaskDates>
-                    <AnnotatedCheckbox checked={completed} onChange={handleCompletedChange}  label="completed" id={props.id} size={20}/>
-                </TaskFooter>
-            </BoxItem>
-        </BlurBox>
-    );
-}
 
 function LoginPage() {
     document.title = "Login";
